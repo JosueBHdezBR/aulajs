b35f5e567a3ee3a0bf97620ec49242c579ca8f47
Josué Benjamín Hernãndez <70042859+JosueBHdezBR@users.noreply.github.com>
2020-08-28 02:03:41 -0300

    changes on main script and html doc

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function click(){
+   document.getElementById("thanking").innerHTML = "Thanks for Clicking";
+}
+function redirection(){
+    window.open("https://github.com/JosueBHdezBR/aulajs");
+    //window.location.href = "https://github.com/JosueBHdezBR/aulajs";
+}
